b86a165ab0934f190d8605c6d01072a9003c7316
alexander annies <alexander.annies@swisscom.com>
2025-10-14 08:20:20 +0200

    feat(Error) Error Page added

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import NavBar from "./Navbar";
+
+function ErrorBoundary() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <NavBar button={true} />
+      <div className="flex flex-col items-center justify-center min-h-screen text-white">
+        <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+        <h2 className="text-2xl font-semibold mb-2">Seite nicht gefunden</h2>
+        <p className="text-gray-400 mb-8 text-center max-w-md">
+          Die Seite, die du suchst, existiert nicht oder wurde verschoben.
+        </p>
+
+        <button
+          onClick={() => navigate("/")}
+          className="px-6 py-3 bg-blue-600 hover:bg-blue-700 transition rounded-lg shadow-md font-medium"
+        >
+          Zurück zur Startseite
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ErrorBoundary;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ContentPage from "./components/ContentPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 //For Routing like /scoreboard
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/page", element: <ContentPage /> },
+  { path: "*", element: <ErrorBoundary /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
